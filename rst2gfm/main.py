c8896ab0a50fd765869eeb2b1c8a3a81d5f95d3b
pdh <1621357+pdh@users.noreply.github.com>
2025-03-08 19:28:09 -0800

    updating docstring for depart_row

--- rst2gfm/main.py
+++ rst2gfm/main.py
@@ -644,6 +644,11 @@ class MarkdownTranslator(NodeVisitor):
         self.current_row = []
 
     def depart_row(self, node):
+        """
+        Appends the current row to the table data.
+        Args:
+            node object: The node object to be processed.
+        """
         self.table_data.append(self.current_row)
 
     def visit_entry(self, node):
